b3c7b15838486b156593fb7ffdc06df6651072e6
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-15 12:08:39 -0700

    1-print_numbers.c

--- main.h
+++ main.h
@@ -4,7 +4,13 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int _puts(char *st);
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_numers(va_list count, int *result);
+
 #endif
