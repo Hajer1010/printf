49a2d670150fd1eae53c9d608aa6179da297247d
hajer <hajeer_00@hotmail.com>
2023-10-15 09:03:24 -0700

    done

--- 0-print_string.c
+++ 0-print_string.c
@@ -9,11 +9,9 @@ int _printf(const char *format, ...)
 	int result = 0, n = 0;
 	va_list convert;
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return (-1)
-			if (!format || (format[0] == '%' && format[1] == '\0'))
-				return (-1);;
 	va_start(convert, format);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 	while (format[n] != '\0')
 	{
 		if (format[n] != '%')
