b642680d4767d4e0b796f774c69c845fe0a2722e
hajer <hajeer_00@hotmail.com>
2023-10-16 16:10:04 -0700

    done

--- print_int.c
+++ print_int.c
@@ -1,5 +1,4 @@
 #include "main.h"
-int write_number(unsigned int);
 /**
  * print_di - print numbers
  * @convert: argument
@@ -16,29 +15,22 @@ int print_di(va_list convert, int *result)
 	int i;
 
 	if (m == 0)
-	{	(*result) += _putchar('0');
+	{	 _putchar('0');
 	}
 	if (m < 0)
 	{	_putchar('-');
-		m = -m;
-		(*result) += m; }
+		m = -m;  }
 	while (rep != 0)
 	{	rep = rep / 10;
-		(*result) += rep;
 		len++; }
 	for (i = 1; i < len; i++)
-	{	po *= 10;
-		(*result) += po; }
+	{	po *= 10; }
 	while (len > 0)
 	{	int di = m / po;
 
 		_putchar(di + '0');
-		(*result) += di;
 		m = m % po;
-		(*result) += m;
 		po = po / 10;
-		(*result) += po;
 		len--; }
-	(*result)++;
 	return (*result);
 }
