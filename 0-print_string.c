94415b8cf649543cdca03ee6bbf4debc0e307d8f
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-16 16:17:02 -0700

    r

--- 0-print_string.c
+++ 0-print_string.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	va_list convert;
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return (1);
+		return (-1);
 	va_start(convert, format);
 	while (format[n] != '\0')
 	{
