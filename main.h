eed75504e75825b0cfcac70fb22d760e2465aa68
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-16 06:46:32 -0700

    unsigned_to_binary.c

--- main.h
+++ main.h
@@ -12,6 +12,6 @@ int _puts(char *st);
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_di(va_list convert, int *result);
-int handle_b(unsigned num);
+int handle_b(unsigned int num);
 
 #endif
