69d63eedf5be4bae873b75fb04bf29456d84e584
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-17 17:41:57 -0700

    print_rev.c

--- print_rev.c
+++ print_rev.c
@@ -8,14 +8,20 @@
 
 int rev_str(char *str)
 {
+	int ad = 0;
 	int ln = 0;
 	int j;
+
+	if (str == NULL)
+		return (_puts("(null)"));
 
 	while (str[ln] != '\0')
 		ln++;
 
 	for (j = ln - 1; j >= 0; j--)
+	{
 		_putchar(str[j]);
-
-	return (ln);
+		ad++;
+	}
+	return (ad);
 }
